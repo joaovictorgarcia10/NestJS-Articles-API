fbc7bcf395844a3b07bf8b8c34a7136130a77470
João Victor Garcia <jvictor_sk8@hotmail.com>
2025-03-04 10:45:51 -0300

    add swagger on dtos

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsString } from "class-validator";
 
 export class CreateArticleDto {
     @IsString()
+    @ApiProperty()
     title: string;
 
     @IsString()
+    @ApiProperty()
     description: string;
 
     @IsNumber({}, { each: true })
+    @ApiProperty()
     categories: number[];
 }
